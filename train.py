d2dbfb49f2500184be972c6d3e42e0b847ae4a4a
gang <jerry050512@outlook.com>
2025-07-23 08:46:03 +0800

    Update mdsam.py

--- train.py
+++ train.py
@@ -5,13 +5,15 @@ import torch
 from tqdm import tqdm
 import os
 import torch.nn.functional as F
+import cv2
+import shutil
+import numpy as np
 from collections import OrderedDict
-import torch.distributed as dist
 import time
 from utils.loss import LossFunc
 from utils.AvgMeter import AvgMeter
 
-dist.init_process_group(backend="nccl")
+from config import train_cfg, test_cfg
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Simple example of a training script.")
@@ -22,7 +24,7 @@ def parse_args():
         "--warmup_period", type = int, default = 5,    
     )
     parser.add_argument(
-        "--batch_size", type = int, default = 1, required = True    
+        "--batch_size", type = int, default = 1,
     )
     parser.add_argument(
         "--num_workers", type = int, default = 0
@@ -37,43 +39,35 @@ def parse_args():
         "--img_size", type = int, default = 512
     )
     parser.add_argument(
-        "--data_path", type = str, required = True, help="the postfix must to be DUTS"
+        "--data_path", type = str, default='./datasets/DUTS', help="the postfix must to be DUTS"
     )
     parser.add_argument(
         "--sam_ckpt", type = str
     )
     parser.add_argument(
-        "--save_dir", type = str, default = "output/"
+        "--save_dir", type = str, default = './output'
     )
     parser.add_argument(
         "--resume", type = str, default = "", help="If you need to train from begining, make sure 'resume' is empty str. If you want to continue training, set it to the previous checkpoint."
     )
-    parser.add_argument(
-        "--local-rank", type=int, default=-1, help="For distributed training: local_rank"
+    parser.add_argument( # 新增一个参数来指定GPU
+        "--gpu_id", type=int, default=0, help="GPU ID to use for training."
     )
 
     args = parser.parse_args()
-    env_local_rank = int(os.environ.get("LOCAL_RANK", -1))
-    if env_local_rank != -1 and env_local_rank != args.local_rank:
-        args.local_rank = env_local_rank
 
     return args
 
-def trainer(net, dataloader, loss_func, optimizer, local_rank):
+def trainer(net, dataloader, loss_func, optimizer):
     net.train()
     loss_avg = AvgMeter()
     mae_avg = AvgMeter()
-    if local_rank == 0:
-        print("start trainning")
+    print("start trainning")
     
     start = time.time()
 
     sigmoid = torch.nn.Sigmoid()
-    if local_rank == 0:
-        data_generator = tqdm(dataloader)
-    else:
-        data_generator = dataloader
-    for data in data_generator:
+    for data in tqdm(dataloader):
         img = data["img"].to(device).to(torch.float32)
         label = data["mask"].to(device).unsqueeze(1)
         
@@ -99,40 +93,41 @@ def trainer(net, dataloader, loss_func, optimizer, local_rank):
         optimizer.step()
     
     temp_cost=time.time()-start
-    print("local_rank:{}, loss:{}, mae:{}, cost_time:{:.0f}m:{:.0f}s".format(local_rank, loss_avg.avg, mae_avg.avg, temp_cost//60, temp_cost%60))
+    print("loss:{}, mae:{}, cost_time:{:.0f}m:{:.0f}s".format(loss_avg.avg, mae_avg.avg, temp_cost//60, temp_cost%60))
  
-def valer(net, dataloader, local_rank):
+def valer(net, dataloader, epoch):
     net.eval()
-    if local_rank == 0:
-        print("start valling")
+    print("start valling")
+
+    save_path = test_cfg.save_root / f"{test_cfg.model_code}-{epoch}" / f"{test_cfg.dataset}"
+    save_path.mkdir(parents=True, exist_ok=True)
 
     start = time.time()
 
     sigmoid = torch.nn.Sigmoid()
     mae_avg = AvgMeter()
     with torch.no_grad():
-        if local_rank == 0:
-            data_generator = tqdm(dataloader)
-        else:
-            data_generator = dataloader
-        for data in data_generator:
+        for data in tqdm(dataloader, desc=f"Validating epoch {epoch}"):
                 
             img = data["img"].to(device).to(torch.float32)
             ori_label = data['ori_mask'].to(device)
+            mask_name = data['mask_name'][0]
             
             out, coarse_out= net(img)
             out = sigmoid(out)
             out = torch.nn.functional.interpolate(out, [ori_label.shape[1],ori_label.shape[2]], mode = 'bilinear', align_corners = False)
 
-            #Since the float values are converted to int when saving the mask, 
-            #multiple decimal will be lost, which may result in minor deviations from the evaluation code.
+            # Since the float values are converted to int when saving the mask,
+            # multiple decimal will be lost, which may result in minor deviations from the evaluation code.
             img_mae=torch.mean(torch.abs(out - ori_label))
 
             mae_avg.update(img_mae.item(),n=1)
-    
-    temp_cost=time.time() - start
 
-    print("local_rank:{}, val_mae:{}, cost_time:{:.0f}m:{:.0f}s".format(local_rank, mae_avg.avg, temp_cost//60, temp_cost%60))
+            pred = (out.squeeze().cpu().numpy() * 255).astype(np.uint8)
+            cv2.imwrite(str(save_path / mask_name), pred)
+
+    temp_cost=time.time() - start
+    print("val_mae:{}, cost_time:{:.0f}m:{:.0f}s".format(mae_avg.avg, temp_cost//60, temp_cost%60))
 
     return mae_avg.avg
 
@@ -191,20 +186,21 @@ def load(net,ckpt, img_size):
 if __name__ == "__main__":
 
     args = parse_args()
-    if args.local_rank == 0:
-        print("start training, batch_size: {}, lr_rate: {}, warmup_period: {}, save to {}".format(args.batch_size, args.lr_rate, args.warmup_period, args.save_dir))
+    args = train_cfg.load_opt(args)
+    test_cfg.load_opt(args)
+
+    print("start training, batch_size: {}, lr_rate: {}, warmup_period: {}, save to {}".format(args.batch_size, args.lr_rate, args.warmup_period, args.ckpt_root))
     torch.manual_seed(args.seed)
-    device = torch.device(f"cuda:{args.local_rank}")
+    device = torch.device(f"cuda:{args.gpu_id}")
 
     #Model definition and loading SAM pre-trained weights
     net = MDSAM(args.img_size).to(device)
-    if args.resume == "":
+    if args.resume != "":
         state = load(net, args.sam_ckpt, args.img_size)
-        if args.local_rank == 0:
-            print(state)
+        print(state)
 
-    trainLoader = getSODDataloader(args.data_path, args.batch_size, args.num_workers, 'train', img_size= args.img_size)
-    valLoader = getSODDataloader(args.data_path, 1, args.num_workers, 'test', args.local_rank, img_size= args.img_size, max_rank = dist.get_world_size())
+    trainLoader = getSODDataloader(train_cfg, img_size= args.img_size)
+    valLoader = getSODDataloader(test_cfg, img_size= args.img_size)
 
     loss_func = LossFunc
 
@@ -253,10 +249,7 @@ if __name__ == "__main__":
             else:
                 net_dict[k] = v
         state = net.load_state_dict(net_dict)
-        if args.local_rank == 0:
-            print(state)
-
-    net=torch.nn.parallel.DistributedDataParallel(net.to(device),device_ids=[args.local_rank],output_device=args.local_rank,find_unused_parameters=True)
+        print(state)
 
     for i in range(start_epoch, args.epochs + 1):
 
@@ -274,33 +267,25 @@ if __name__ == "__main__":
                 param_group['lr'] = _lr * 0.1
             t += 1
         
-        if args.local_rank == 0:
-            print("epochs {} start".format(i))
+        print("epochs {} start".format(i))
 
-        trainer(net, trainLoader, loss_func, optimizer, local_rank=args.local_rank)
+        # trainer(net, trainLoader, loss_func, optimizer)
 
-        local_mae = valer(net, valLoader, local_rank = args.local_rank)
+        mae = valer(net, valLoader, i)
 
-        #average the results from multi-GPU inference
-        sum_result = torch.tensor(local_mae).to(device)
-        dist.reduce(sum_result, dst = 0, op = dist.ReduceOp.SUM)
+        print("current mae:{}".format(mae))
+        rm_epoch = i
+        #save the best result
+        if mae < best_mae:
+            best_mae = mae
+            rm_epoch = best_epoch
+            best_epoch = i
+        pred_dir_to_remove = test_cfg.save_root / f"{test_cfg.model_code}-{rm_epoch}"
+        if pred_dir_to_remove.exists():
+            shutil.rmtree(pred_dir_to_remove)
 
-        if args.local_rank == 0:
-            mae = sum_result.item() / dist.get_world_size()
-            print("current mae:{}".format(mae))
-            #save the best result
-            if(mae < best_mae):
-                best_mae = mae
-                best_epoch = i
-                print("save epoch {} in {}".format(i, "{}/model_epoch{}.pth".format(args.save_dir,i)))
-                if not os.path.exists(args.save_dir):
-                    os.makedirs(args.save_dir)
-                torch.save({"model": net.state_dict(),"optimizer":optimizer.state_dict()}, "{}/model_epoch{}.pth".format(args.save_dir,i))
-            print("best epoch:{}, mae:{}".format(best_epoch,best_mae))
+        print("save epoch {} in {}".format(i, "{}/{}-{}.pth".format(args.ckpt_root, args.model_code, i)))
+        args.ckpt_root.mkdir(parents=True, exist_ok=True)
+        torch.save({"model": net.state_dict(),"optimizer":optimizer.state_dict()}, "{}/{}-{}.pth".format(args.ckpt_root, args.model_code, i))
+        print("best epoch:{}, mae:{}".format(best_epoch,best_mae))
         
-
-
-
-
-
-
